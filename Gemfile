83b525dbe3d0c5f26840268be2b926d1d807a6e3
Tarkesh-Parikh <Tarkeshkumarp@gmail.com>
2015-10-27 21:24:10 -0400

    Cucumber-RWebDriver
    Cucumber-RWebDriver

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in Cucumber-RWebDriver.gemspec
+gemspec
---
+++ lib/Cucumber/RWebDriver.rb
@@ -0,0 +1,7 @@
+require "Cucumber/RWebDriver/version"
+
+module Cucumber
+  module RWebDriver
+    # Your code goes here...
+  end
+end
